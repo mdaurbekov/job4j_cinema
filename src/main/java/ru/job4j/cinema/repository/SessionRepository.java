1537ea1b620b576f5b072474f4aa4e85b11237b5
mdaurbekov <dreamkov@gmail.com>
2022-12-12 20:09:13 +0300

    Implemented SessionRepository

---
+++ src/main/java/ru/job4j/cinema/repository/SessionRepository.java
@@ -0,0 +1,65 @@
+package ru.job4j.cinema.repository;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
+import ru.job4j.cinema.model.Session;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class SessionRepository {
+    private static final Logger LOG = LogManager.getLogger(SessionRepository.class.getName());
+    private static final String ADD = "INSERT INTO session(name) VALUES (?)";
+    private static final String FIND_ALL = "SELECT * FROM session";
+    private BasicDataSource pool;
+
+    public SessionRepository(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public Session add(Session session) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement(ADD,
+                     PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, session.getName());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    session.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return session;
+    }
+
+    public List<Session> findAll() {
+        List<Session> sessions = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement(FIND_ALL)
+        ) {
+            try (ResultSet it = ps.executeQuery()) {
+                while (it.next()) {
+                    sessions.add(getSession(it));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return sessions;
+    }
+
+    private Session getSession(ResultSet it) throws SQLException {
+        return new Session(it.getInt("id"), it.getString("name"));
+    }
+
+}
